0644f31a578dc48acd92af2efc8249b44d90764a
Paweł Jadach <pawel.jadach.96@gmail.com>
2020-02-19 18:05:52 +0100

    change styles

--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -5,11 +5,11 @@ import styles from './PageNav.module.scss';
 const PageNav = () => {
   return (
     <nav>
-      <NavLink exact to={process.env.PUBLIC_URL + '/'} activeClassName={styles.active}>Dashboard</NavLink>
-      <NavLink exact to={process.env.PUBLIC_URL + '/kitchen'} activeClassName={styles.active}>Kitchen</NavLink>
-      <NavLink exact to={process.env.PUBLIC_URL + '/login'} activeClassName={styles.active}>Login</NavLink>
-      <NavLink exact to={process.env.PUBLIC_URL + '/tables'} activeClassName={styles.active}>Tables</NavLink>
-      <NavLink exact to={process.env.PUBLIC_URL + '/waiter'} activeClassName={styles.active}>Waiter</NavLink>
+      <NavLink exact to={process.env.PUBLIC_URL + '/'} activeClassName={styles.active} className={styles.navLink}>Dashboard</NavLink>
+      <NavLink exact to={process.env.PUBLIC_URL + '/kitchen'} activeClassName={styles.active} className={styles.navLink}>Kitchen</NavLink>
+      <NavLink exact to={process.env.PUBLIC_URL + '/login'} activeClassName={styles.active} className={styles.navLink}>Login</NavLink>
+      <NavLink exact to={process.env.PUBLIC_URL + '/tables'} activeClassName={styles.active} className={styles.navLink}>Tables</NavLink>
+      <NavLink exact to={process.env.PUBLIC_URL + '/waiter'} activeClassName={styles.active} className={styles.navLink}>Waiter</NavLink>
     </nav>
   );
 };
